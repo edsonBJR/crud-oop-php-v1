883baf1458b460fe1dd975c790bc2fc338853e15
edsonBJR <ebjr.tux@gmail.com>
2020-11-20 15:52:20 -0300

    Delete function is working.

--- objects/product.php
+++ objects/product.php
@@ -132,5 +132,18 @@ class Product{
 
         return false;
     }
+
+    function delete() {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+
+        if($result = $stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 ?>
